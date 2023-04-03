0777914b2296d0db15728a6946b9e6cc1d16c4a6
1190190 <fayaaz.nabie@hva.nl>
2023-04-03 21:25:32 +0200

    .

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
-console.log("howdy");
+var form = document.querySelector (".NaamCustom");
+
+form.addEventListener("submit", (event) => {
+    var Pepsinaam = document.querySelector("input").value;
+    event.preventDefault();
+        document.querySelector('.naam').textContent =  Pepsinaam;
+    
+})
+
+
